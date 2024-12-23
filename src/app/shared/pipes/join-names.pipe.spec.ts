246d8cdf7909341cf169192a9d16d4f85c704963
mykolamatiash <nickolay.matiash@boosta.co>
2024-12-23 13:27:58 +0100

    delete tests

--- src/app/shared/pipes/join-names.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { JoinNamesPipe } from './join-names.pipe';
-
-describe('JoinNamesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new JoinNamesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
